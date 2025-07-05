cd5d3421927badb9bd8ad607fae8accb678f02f4
sans-creator <sanskarj163@gmail.com>
2025-07-05 20:51:42 +0530

    generating data

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const mysql = require("mysql2");
 const { faker } = require("@faker-js/faker");
 
+
+
+let connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Shipra@123", 
+  database: "sans",
+});
+
 let getRandomUser = () => {
   return [
     faker.string.uuid(),
@@ -10,22 +19,20 @@ let getRandomUser = () => {
   ];
 };
 
-// console.log(getRandomUser());
+let q = "INSERT INTO user(id, username, email, password) VALUES ?";
 
-let connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Shipra@123", 
-  database: "sans",
-});
-try{
-  connection.query("SHOW TABLES", (err, result) => {
-  if (err) throw err;
-  console.log(result);
-});
-
-
-} catch(err){
-  console.log(err)
+let data=[];
+for(let i=1;i<=100;i++){
+  data.push(getRandomUser()) //100 fake users using faker
 }
-connection.end()
+
+connection.query(q, [data], (err, result) => {
+  if (err) {
+    console.error("Error inserting data:", err);
+  } else {
+    console.log("Insert successful:", result);
+  }
+  connection.end(); // Ensure connection closes after query
+});
+
+
